7439e0b6e07299c4c8a5c87f18b671e49820c41a
Fernando Morrison <fmorri@gmail.com>
2025-01-04 18:00:14 +0100

    Working fine. Set latency to 20ms

--- index.js
+++ index.js
@@ -33,63 +33,70 @@ app.get("/", async (req, res) => {
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
-  }, 100);
+  }, 20);
 });
 
 app.post("/", showReq, async (req, res) => {
   const { label } = req.body;
 
-  try {
-    const newTask = new Task({
-      label: label,
-      isDone: false,
-      isArchived: false,
-    });
+  setTimeout(async () => {
+    try {
+      const newTask = new Task({
+        label: label,
+        isDone: false,
+        isArchived: false,
+      });
 
-    await newTask.save();
+      await newTask.save();
 
-    console.log("New task created", newTask);
+      console.log("New task created", newTask);
 
-    res.status(200).json({ message: "All good." });
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
+      res.status(200).json({ message: "All good." });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }, 20);
 });
 
 app.put("/", showReq, async (req, res) => {
   const { label, isDone, isArchived, id } = req.body;
 
-  try {
-    const taskToUpdate = await Task.findById(id);
+  setTimeout(async () => {
+    try {
+      const taskToUpdate = await Task.findById(id);
 
-    console.log("taskToUpdate is", taskToUpdate);
+      console.log("taskToUpdate is", taskToUpdate);
 
-    isDone !== undefined && (taskToUpdate.isDone = isDone);
-    isArchived !== undefined && (taskToUpdate.isArchived = isArchived);
+      isDone !== undefined && (taskToUpdate.isDone = isDone);
+      isArchived !== undefined && (taskToUpdate.isArchived = isArchived);
 
-    console.log("Task updated", taskToUpdate);
+      console.log("Task updated", taskToUpdate);
 
-    await taskToUpdate.save();
+      await taskToUpdate.save();
 
-    res.status(200).json(taskToUpdate);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
+      res.status(200).json(taskToUpdate);
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }, 20);
 });
 
 app.delete("/:id", showReq, async (req, res) => {
   const { id } = req.params;
-  try {
-    const taskToDelete = await Task.findOneAndDelete({
-      _id: id,
-    });
+  setTimeout(async () => {
+    try {
+      const taskToDelete = await Task.findOneAndDelete({
+        _id: id,
+      });
 
-    console.log("Task deleted", taskToUpdate);
+      console.log("Task deleted", taskToDelete);
 
-    res.status(200).json({ message: `task deleted.` });
-  } catch (error) {
-    res.status(500).json({ meessage: error.message });
-  }
+      res.status(200).json({ message: `task deleted.` });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ meessage: error.message });
+    }
+  }, 20);
 });
 
 app.get("*", showReq, (req, res) => {
